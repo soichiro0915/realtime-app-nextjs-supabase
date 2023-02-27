3cfa08e38b3f68ff36a01f6773c30f13f790902e
soichiro0915 <soichiro.ex.eru0915@gmail.com>
2023-02-27 17:18:00 +0900

    create next app and set init

---
+++ pages/_app.tsx
@@ -0,0 +1,23 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import { ReactQueryDevtools } from 'react-query/devtools'
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false, // フォーカス時にリフェッチしない
+      retry: false, // fetch失敗時３回までリトライするが、それをやめる
+      suspense: true,
+    },
+  },
+})
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Component {...pageProps} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  )
+}
